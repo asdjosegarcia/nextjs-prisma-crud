caf8f0d0143c127cdb3b5a541d7c2d42675689c2
asdjosegarcia <asdjosegarcia@gmail.com>
2023-10-09 21:24:18 -0300

    actualizar tareas

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,24 +1,52 @@
 "use client"
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 
-const NewPage =  () => {
-  const router=useRouter()
-  const onSubmit=async (e)=>{
-    e.preventDefault()
-    const title=e.target.title.value
-    const description=e.target.description.value
-    const res= await fetch('/api/tasks',{//no hace falta agregar en endpoint de la api, el navegador la autocompleta
-      method:'POST',
-      body:JSON.stringify({title,description}), //transformamos el titulo y descripcion a JSON
-      headers:{
-        'Content-Type':'application/json'
-      }
-    })
-    const data=await res.json()
-    console.log(data)
-    router.push("./");
-    
+const NewPage = ({ params }) => {//recimos el parametro de la url ej el ID
+  const router = useRouter();//alamacenamos el router
+  const [title, setTitle] = useState("")//el titulo de la tarea
+  const [description, setDescription] = useState("")
+
+  useEffect(() => {// cuando carga la pag vamos a mostrar los datos de la tarea.
+    if (params.id) { //si existe el params.id extremos los datos y los colocamos en el input
+      fetch(`/api/tasks/${params.id}`)//realizamos una peticion get a parametro de la url.id
+        .then(res => res.json())//tranformamos la respuesta a json y almacenamos en data
+        .then(data => { 
+          setTitle(data.title), // extraemos titulo y descripcion de  la tarea para almacenarlo en un useState para establecerlo como valor por defecto de los inputs
+          setDescription(data.description) 
+          // console.log(description)
+        })
+    }
+  }, [])
+
+  const onSubmit = async (e) => {//e 
+    e.preventDefault()//evitamos la accion por defecto, que seria recargar la pagina, e tiene los datos del input
+    // const title=e.target.title.value//valor del titulo que extraemos cuando le damos a subir
+    if (params.id) {//si la tarea existe hacemos una peticion put para reemplazar los datos
+      const res=await fetch(`/api/tasks/${params.id}`,{
+        method:'PUT',
+        body: JSON.stringify({title,description}),
+        headers:{
+          "Content-Type": "application/json",//json
+        }
+      })
+      const data =await res.json();
+      console.log(data)
+    } else {//si la tarea no existe, la creamos
+      const res = await fetch("/api/tasks", {
+        method: "POST",//creamos la tarea con POST
+        body: JSON.stringify({ title, description }),//enviamos como datos para la nueva tarea un post con titulo y descripcion
+        headers: { //le indicamos que trabajaremos con 
+          "Content-Type": "application/json",//json
+        },
+      })
+      const data = await res.json();//datos que contienen la tarea
+      console.log(data)
+    }
+    router.refresh()//actualiza toda la cache (recarga la pagina)
+    router.push("/");//una vez creada o modifica la tarea redireccionamos a el usuario a el home
+
   }
 
   return (
@@ -31,9 +59,15 @@ const NewPage =  () => {
           id="title"
           className="border border-gray-400 p-2 mb-4 w-full text-black"//border, borde gris, pading de 4, margin button 4, todo el ancho
           placeholder="Título"
+          onChange={(e) => setTitle(e.target.value)}//onCahnge ejecuta cuando hay cmabios, establecemos el useState title con el valor del input
+          defaultValue={title}//establecemos el valor inicial com title
         />
         <label htmlFor="description" className="font-bold text-sm">Descripcion de la tarea</label>
-        <textarea id="description" rows="3" className="border border-gray-400 p-2 mb-4 w-full text-black" placeholder="Describe tu tarea"></textarea>
+        <textarea id="description" rows="3" className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Describe tu tarea"
+          onChange={(e) => setDescription(e.target.value)}//establecemos el valor del la descripcion
+          defaultValue={description}//establecemos el valor inicial como description
+        ></textarea>
         <button className="bg-blue-500 hover:bg-blue-700 text-white rounded py-2 px-4">Crear</button>{/* rounded border-radius */}
       </form>
     </div>
